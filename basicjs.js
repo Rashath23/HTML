b335f13cfeb4c6a583b82ae6e7114e6055c75b71
Rashath A <146910342+Rashath23@users.noreply.github.com>
2025-10-13 11:24:23 +0530

    Create basicjs.js

---
+++ basicjs.js
@@ -0,0 +1,8 @@
+
+function showMessage() {
+  document.getElementById("message").innerHTML = "Hello JS! Welcome Sham Sundar G 😊";
+  
+  console.log("Button clicked!");
+  console.log("Name: Sham Sundar G");
+  console.log("Age: 25");
+}
